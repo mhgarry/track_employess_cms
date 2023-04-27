4b27cc624ae509bd462c4dfcb6814f4a4bb28721
Matt <mhgarry92@gmail.com>
2023-04-27 18:28:02 -0400

    updated schema.sql added seeds.sql

--- config/schema.sql
+++ config/schema.sql
@@ -3,23 +3,25 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE departments (
-	id INT NOT NULL PRIMARY KEY,
+CREATE TABLE department (
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-	id INT NOT NULL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL,
+CREATE TABLE role (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
 	salary DECIMAL NOT NULL,
-	department_id INT NOT NULL,
-	FOREIGN KEY (department_id) REFERENCES departments(id)
+	department_id INT,
+	FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
-	role_id INT NOT NULL,
-	FOREIGN KEY (role_id) REFERENCES roles(id)
-)
+	role_id INT,
+	manager_id INT,
+	FOREIGN KEY (role_id) REFERENCES role(id),
+	FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
